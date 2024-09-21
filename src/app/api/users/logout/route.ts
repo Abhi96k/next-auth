4dca06f3f215c75bd7d7a98ad488a6a9b339dec4
abhi@96k <abhisheknangare96k@gmail.com>
2024-09-22 01:20:44 +0530

    Refactor logout route to simplify response handling and improve success message

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -1,45 +1,20 @@
-import { connect } from "@/dbConfig/dbConfig";
-import User from "@/models/userModel";
-import { NextRequest, NextResponse } from "next/server";
-import bcryptjs from "bcryptjs";
-import jwt from "jsonwebtoken";
-
-connect();
-
-export async function GET(request: NextRequest) {
-  try {
-    const response = NextResponse.json(
-      {
-        message: "User logged out",
-      },
-      {
-        status: 200,
-      }
-    );
+import { NextResponse } from "next/server";
 
 
-    response.cookies.set("token", "", {
-      httpOnly: true,
-    });
-    
-
-    return NextResponse.json(
-      {
-        message: "User logged out",
-      },
-      {
-        status: 200,
-      }
-    );
-  } catch (error: any) {
-    console.error("user does not logout in server", error);
-    return NextResponse.json(
-      {
-        error: error.message,
-      },
-      {
-        status: 500,
-      }
-    );
-  }
-}
+export async function GET() {
+    try {
+        const response = NextResponse.json(
+            {
+                message: "Logout successful",
+                success: true,
+            }
+        )
+        response.cookies.set("token", "", 
+        { httpOnly: true, expires: new Date(0) 
+        });
+        return response;
+    } catch (error: any) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+        
+    }
