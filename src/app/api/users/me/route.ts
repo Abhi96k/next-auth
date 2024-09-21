b3b894ee064593cc283d92211c2f86dbaf63ba8d
abhi@96k <abhisheknangare96k@gmail.com>
2024-09-22 02:02:55 +0530

    Implement user retrieval in POST route with authorization checks and error handling

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -3,9 +3,43 @@ import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
 import { sendEmail } from "@/helper/mailer";
+import { getDataFromToken } from "@/helper/getDataFromToken";
 
 connect();
 
-export async function POST(request:){
+export async function POST(request: NextRequest) {
+  const userId = await getDataFromToken(request);
 
+  if (!userId) {
+    return NextResponse.json(
+      {
+        error: "Unauthorized",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+
+  const user = await User.findById({ _id: userId }).select("-password");
+
+  if (!user) {
+    return NextResponse.json(
+      {
+        error: "User not found",
+      },
+      {
+        status: 404,
+      }
+    );
+  }
+
+  return NextResponse.json(
+    {
+      user,
+    },
+    {
+      status: 200,
+    }
+  );
 }
