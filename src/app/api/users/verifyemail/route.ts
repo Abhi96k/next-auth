6ccef8ccfaf60420ec7457be4bde178ccf6c3e6c
abhi@96k <abhisheknangare96k@gmail.com>
2024-09-21 22:10:06 +0530

    Implement email verification endpoint with error handling and token validation

---
+++ src/app/api/users/verifyemail/route.ts
@@ -0,0 +1,46 @@
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { token } = reqBody;
+    console.log("Token:- ", token);
+
+    const user = await User.findOne({
+      verifyToken: token,
+      verifyTokenExpiry: { $gt: Date.now() },
+    });
+
+    if (!user) {
+      return NextResponse.json(
+        { message: "Invalid or expired token" },
+        { status: 400 }
+      );
+    }
+
+    user.isVerified = true;
+    user.verifyToken = undefined;
+    user.verifyTokenExpiry = undefined;
+
+    await user.save();
+
+    return NextResponse.json(
+      {
+        message: "Email verified",
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json(
+      { message: "Something went wrong email is not verified" },
+      { status: 500 }
+    );
+  }
+}
