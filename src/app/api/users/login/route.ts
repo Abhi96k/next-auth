62f804cfee767d7ace32d59ccb656f96eb10c9c2
abhi@96k <abhisheknangare96k@gmail.com>
2024-09-22 00:10:33 +0530

    Refactor login route for improved error handling and response formatting

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -14,29 +14,46 @@ export async function POST(request: NextRequest) {
 
     //check if user exists
     const user = await User.findOne({ email });
-    
+
     if (!user) {
       return NextResponse.json(
-        { error: "User does not exist" },
-        { status: 400 }
+        {
+          error: "User does not exist",
+        },
+        {
+          status: 400,
+        }
       );
     }
+    
     console.log("user exists");
 
     //check if password is correct
     const validPassword = await bcryptjs.compare(password, user.password);
+
     if (!validPassword) {
-      return NextResponse.json({ error: "Invalid password" }, { status: 400 });
+      return NextResponse.json(
+        {
+          error: "Invalid password",
+        },
+        {
+          status: 400,
+        }
+      );
     }
+
     console.log(user);
 
     //create token data
+
     const tokenData = {
       id: user._id,
       username: user.username,
       email: user.email,
     };
+
     //create token
+
     const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
       expiresIn: "1d",
     });
@@ -45,11 +62,20 @@ export async function POST(request: NextRequest) {
       message: "Login successful",
       success: true,
     });
+
     response.cookies.set("token", token, {
       httpOnly: true,
     });
+
     return response;
   } catch (error: any) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
+    return NextResponse.json(
+      {
+        error: error.message,
+      },
+      {
+        status: 500,
+      }
+    );
   }
 }
