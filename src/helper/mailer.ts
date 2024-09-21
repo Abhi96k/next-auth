9bc846f0410fd2fb58410614768c976b4660573a
abhi@96k <abhisheknangare96k@gmail.com>
2024-09-21 18:43:30 +0530

    Update email transporter configuration to use SMTP settings and enhance email options

--- src/helper/mailer.ts
+++ src/helper/mailer.ts
@@ -1,10 +1,26 @@
+import { Html } from "next/document";
 import nodemailer from "nodemailer";
 
 export const sendEmail = async ({ email, emailType, userID }) => {
   try {
     const transporter = nodemailer.createTransport({
-      host:"sandbox"
+      host: "smtp.forwardemail.net",
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
     });
+
+    const mailOptions = {
+      from: "abhisheknangare96k@gmail.com",
+      to: email,
+      subject:
+        emailType === "VERIFY" ? "Verify your email" : "Reset your password",
+      text: "Hello world?",
+      html: "<b>Hello world?</b>",
+    };
   } catch (err) {
     console.error(err);
     console.log("Error sending email");
