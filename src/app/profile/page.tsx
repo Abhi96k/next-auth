08bfcbf90dc0136225b7995a5d1738eeb074d5aa
abhi@96k <abhisheknangare96k@gmail.com>
2024-09-22 13:51:05 +0530

    Enhance ProfilePage with user details retrieval and logout functionality

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,31 +1,60 @@
-import React from "react";
+"use client";
 import axios from "axios";
 import Link from "next/link";
+import React, { useState } from "react";
 import { toast } from "react-hot-toast";
-import { useRouter } from "next/router";
-
+import { useRouter } from "next/navigation";
 
 export default function ProfilePage() {
   const router = useRouter();
-  const [data, setData] = React.useState("nothing");
-
-
-  const getUserDetails = async () => {
-    const res = await axios.post("/api/user/me");
-    console.log(res.data);
-    setData(res.data);
-  };
+  const [data, setData] = useState("nothing");
 
   const logout = async () => {
-    try{
+    try {
       await axios.get("/api/users/logout");
-      toast.success("Logged out successfully");
+      toast.success("Logout successful");
       router.push("/login");
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
     }
-    catch(error){
-      console.log(error);
-      toast.error("Failed to logout");
-    }
+  };
 
-  return <div>Profile Page</div>;
-};
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/me");
+    console.log(res.data);
+    setData(res.data.data._id);
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-white">
+      <h1 className="text-4xl font-bold mb-6">Profile Page</h1>
+      <hr className="w-full max-w-md mb-6 border-white" />
+
+      <h2 className="p-4 rounded-lg bg-white text-black text-2xl mb-4">
+        {data === "nothing" ? (
+          "No data yet!"
+        ) : (
+          <Link href={`/profile/${data}`}>{data}</Link>
+        )}
+      </h2>
+      <hr className="w-full max-w-md mb-6 border-white" />
+
+      <div className="flex space-x-4">
+        <button
+          onClick={logout}
+          className="bg-blue-500 hover:bg-blue-700 transition duration-300 ease-in-out text-white font-bold py-2 px-4 rounded-lg shadow-lg"
+        >
+          Logout
+        </button>
+
+        <button
+          onClick={getUserDetails}
+          className="bg-green-600 hover:bg-green-700 transition duration-300 ease-in-out text-white font-bold py-2 px-4 rounded-lg shadow-lg"
+        >
+          Get User Details
+        </button>
+      </div>
+    </div>
+  );
+}
