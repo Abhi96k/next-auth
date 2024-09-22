3ff74115522ced66575baefa51221d8255d0c1a7
abhi@96k <abhisheknangare96k@gmail.com>
2024-09-22 09:55:33 +0530

    Add axios and react-hot-toast for improved signup handling

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import axios from "axios";
+import {toast} from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
 
 export default function SignupPage() {
 
@@ -9,11 +13,20 @@ export default function SignupPage() {
   });
 
   const [buttonDisabled, setButtonDisabled] = React.useState(false);
+  const router = useRouter();
 
   const [loading, setLoading] = React.useState(false);
 
   const onSignup = async () => {
-     
+    try{
+      setLoading(true);
+      const response=await axios.post("/api/users/signup", user);
+      console.log("Signup Success",response.data);
+    }
+    catch(e:any){
+      console.error("Signup Error",e.response.data);
+      toast.error(e.response.data.message);
+    }
   }
 
   return (
